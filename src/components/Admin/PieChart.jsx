31ec346f278af16ccb3801b7b3aca96749da8d86
Bibin A <bibinolavanna@gmail.com>
2023-05-11 10:36:22 +0530

    confirm status added

--- src/components/Admin/PieChart.jsx
+++ src/components/Admin/PieChart.jsx
@@ -4,19 +4,21 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function PieChart({ props }) {
   const data = {
-    labels: ["Cancelled", "Processing", "Pending", "Completed"],
+    labels: ["Cancelled","Confirmed", "Processing", "Pending", "Completed"],
     datasets: [
       {
         label: "Works",
-        data: [props.cancel, props.processing, props.pending, props.works],
+        data: [props.cancel,props.confirm, props.processing, props.pending, props.works],
         backgroundColor: [
           "rgba(255, 99, 132, 0.2)",
+          'rgba(153, 102, 255, 0.2)',
           "rgba(54, 162, 235, 0.2)",
           "rgba(255, 206, 86, 0.2)",
           "rgba(75, 192, 192, 0.2)",
         ],
         borderColor: [
           "rgba(255, 99, 132, 1)",
+          'rgba(153, 102, 255, 1)',
           "rgba(54, 162, 235, 1)",
           "rgba(255, 206, 86, 1)",
           "rgba(75, 192, 192, 1)",
